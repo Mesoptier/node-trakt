9b4f0c563237a037ba4d6da6ec889592ab530549
Koen Klaren <koen.klaren@gmail.com>
2013-10-13 20:48:24 +0200

    Changed initiation

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-var trakt = require("../node-trakt")("db5ecf5582f5886f910a5a9cdc2b5065");
+var trakt = require("../node-trakt");
 
-trakt.login("tube-test", "tube-test", function(loggedIn){
-    console.log("Login: ", loggedIn);
+trakt.init("db5ecf5582f5886f910a5a9cdc2b5065");
 
-    trakt.movieSummary({ title: "the-lone-ranger-2013" }, function(err, data){
+trakt.login("tube-test", "tube-test", function(){
+    trakt.movieSummaries({ title: "the-social-network-2010,tt1483013" }, function(err, data){
         if (err)
             throw err;
         console.log(data);
