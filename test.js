1a2d6420e0c293b2c83b5512044c74456af49156
Koen Klaren <koen.klaren@gmail.com>
2014-07-18 18:07:36 +0200

    Coding style in test file

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
-var trakt = require("../node-trakt");
+var trakt = require("./index.js");
 
 trakt.init("db5ecf5582f5886f910a5a9cdc2b5065");
 
-trakt.login("tube-test", "tube-test", function(){
-    trakt.movieSummaries({ title: "the-social-network-2010,tt1483013" }, function(err, data){
+trakt.login("tube-test", "tube-test", function () {
+    trakt.movieSummaries({ title: "the-social-network-2010,tt1483013" }, function (err, data) {
         if (err)
             throw err;
+
         console.log(data);
     });
 });
