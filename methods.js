9033edeb84dc6e2c8f66a78373a2934c6e075371
Koen Klaren <koen.klaren@gmail.com>
2015-02-10 21:06:53 +0100

    Add more API methods

--- methods.js
+++ methods.js
@@ -1,13 +1,20 @@
 var util = require("./util");
 var path = util.path;
 
-module.exports = {
+var search = {
   search: function (query, type) {
     var qs = { query: query };
     if (type) qs.type = type;
     return this.get("/search", qs);
   },
 
+  searchId: function (type, id) {
+    var qs = { id_type: type, id: id };
+    return this.get("/search", qs);
+  }
+};
+
+var shows = {
   showSummary: function (id, extended) {
     var qs = {};
     if (extended) qs.extended = extended;
@@ -20,5 +27,25 @@ module.exports = {
 
   showTranslations: function (id, language) {
     return this.get(path("/shows/%/translations/%", id, language));
+  },
+
+  showComments: function (id) {
+    return this.get(path("/shows/%/comments", id));
+  },
+
+  showPeople: function (id) {
+    return this.get(path("/shows/%/people", id));
+  },
+
+  showRatings: function (id) {
+    return this.get(path("/shows/%/ratings", id));
+  },
+
+  showRelated: function (id) {
+    return this.get(path("/shows/%/related", id));
   }
 };
+
+module.exports = {};
+util.extend(module.exports, search);
+util.extend(module.exports, shows);
