85ae526ff859259c1302b29307971231eb679f40
Koen Klaren <koen.klaren@gmail.com>
2015-02-24 11:56:30 +0100

    Rewrite util for ES6

--- util.js
+++ util.js
@@ -1,18 +1,13 @@
-module.exports = {
-  extend: extend,
-  path: path
-};
-
-function extend(destination, source) {
-  for (var k in source) {
+export function extend(destination, source) {
+  for (let k in source) {
     if (source.hasOwnProperty(k)) {
       destination[k] = source[k];
     }
   }
 }
 
-function path(path) {
-  var params = [], i;
+export function path(path) {
+  let params = [], i;
   for (i = 1; i < arguments.length; i++)
     params[i - 1] = arguments[i];
 
@@ -20,7 +15,7 @@ function path(path) {
 }
 
 function pathReplace(path, params) {
-  return path.replace(/(\/?)%(.*)$/g, function (match, pre, path) {
+  return path.replace(/(\/?)%(.*)$/g, (match, pre, path) => {
     return params.length && params[0] !== undefined
       ? pre + params.shift() + pathReplace(path, params)
       : "";
